8a431f232762d9d3be76b88a572857e8fa725f52
Venkatesh4697 <40483195+Venkatesh4697@users.noreply.github.com>
2023-11-13 12:29:49 +0530

    tsmarketing initial commit
    Initial commit for tsmarketing scrape

--- main.py
+++ main.py
@@ -45,10 +45,43 @@ def get_prices_rbzts():
             return str(e)
 
 
+def get_prices_tsmarketing():
+    url = "http://tsmarketing.in/DailyArrivalsnPricesCommoditywise.aspx"
+    status_code = url_ok(url)
+    if status_code == 503 or status_code == 502 or status_code == 504 or status_code == 500:
+        return "TsMarketing Service/Website is currently not available"
+    else:
+        try:
+            req = requests.get(url)
+
+            soup = BeautifulSoup(req.content, "html.parser")
+            table = soup.find(
+                lambda tag: tag.name == 'table' and tag.has_attr('id') and tag[
+                    'id'] == "ContentPlaceHolder1_GridView1")
+            rows = table.findAll(lambda tag: tag.name == 'td')
+            prices = []
+            fields = ['Commodity Name', 'Variety Name', 'Market Name', 'Arrivals(Qtls)', 'Maximum', 'Minimum', 'Model',
+                      'Purchase By']
+
+            for td in rows:
+                for child in td.children:
+                    prices.append(child.text.strip())
+
+            it = iterutils.chunked(prices, 8)
+            date = str(datetime.today().strftime('%Y%m%d'))
+            with(open('tsmarketing_prices_report_' + date + '.csv', 'w', newline='')) as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerow(fields)
+                writer.writerows(it)
+            return "Scrape Success. File generated: " + str(csvfile.name)
+        except Exception as e:
+            return str(e)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     rbzts_response = get_prices_rbzts()
     print(rbzts_response)
+    tsmarketing_response = get_prices_tsmarketing()
+    print(tsmarketing_response)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
